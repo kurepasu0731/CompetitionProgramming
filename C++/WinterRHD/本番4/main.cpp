46d968e2e311b8b76e9e56383917b0f43b7cdbe4
Hiroki11x <hiroki11x@gmail.com>
2015-12-08 00:11:04 +0900

    restore file

--- C++/WinterRHD/本番4/main.cpp
+++ C++/WinterRHD/本番4/main.cpp
@@ -1,10 +1,27 @@
 #include<iostream>
+#include <vector>  
+#include <string>  
 using namespace std;
 
+
 int main(void){
-    int a,b,c;
-    cin >> a;
-    cin >> b;
-    cin >> c;
-    return (a+b+c-2);
+    long n, l;
+    long x[100000],a[100000];
+    cin>> n >> l;
+    int i,j;
+    unsigned long distance;
+    for(i=0;i<n;i++){
+        cin >> x[i]>>a[i];
+    }
+    for(i=0;i<n;i++){
+        long sum=0;
+        for(j=0;j<n;j++){
+            distance = labs(x[i]-x[j]);
+            if(2*distance > l){
+                distance = l-distance;
+            }
+            sum+=(distance*a[j]);
+        }
+        cout << sum<< endl;
+    }
 }
