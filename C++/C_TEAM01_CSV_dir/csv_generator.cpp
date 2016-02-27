2482957b84e860e29f4fc06ceed82c964e3b98af
Hiroki11x <hiroki11x@gmail.com>
2016-02-27 20:31:20 +0900

    reindent code

--- C++/C_TEAM01_CSV_dir/csv_generator.cpp
+++ C++/C_TEAM01_CSV_dir/csv_generator.cpp
@@ -15,25 +15,24 @@
 using namespace std;
 
 bool fileExists(const std::string& fileName){
-     std::fstream file;
-     file.open(fileName.c_str(), std::ios::in);
-     if (file.is_open() == true)
-     {
-         file.close();
-         return true;
-     }
-     file.close();
-     return false;
+	std::fstream file;
+	file.open(fileName.c_str(), std::ios::in);
+	if (file.is_open() == true){
+		file.close();
+		return true;
+	}
+	file.close();
+	return false;
 }
 
 
 static void copyFile(const std::string& fileNameFrom, const std::string& fileNameTo){
-     assert(fileExists(fileNameFrom));
-     std::ifstream in (fileNameFrom.c_str());
-     std::ofstream out (fileNameTo.c_str());
-     out << in.rdbuf();
-     out.close();
-     in.close();
+	assert(fileExists(fileNameFrom));
+	std::ifstream in (fileNameFrom.c_str());
+	std::ofstream out (fileNameTo.c_str());
+	out << in.rdbuf();
+	out.close();
+	in.close();
 }
 
 
@@ -47,46 +46,34 @@ const char* path = "./";
 DIR *dp;       // ディレクトリへのポインタ
 dirent* entry; // readdir() で返されるエントリーポイント
 
-string a = "a/";  
-string b = "b/"; 
-string c = "c/"; 
-string A = ",1,0,0";
-string B = ",0,1,0";
-string C = ",0,0,1";
-
-char *src = new char[100];
-char *dst = new char[100];
+string a_path = "a/";  
+string b_path = "b/"; 
+string c_path = "c/"; 
+string a_vector = ",1,0,0";
+string b_vector = ",0,1,0";
+string c_vector = ",0,0,1";
 
 dp = opendir(path);
 if (dp==NULL) exit(1);
 do {
-  entry = readdir(dp);
-  if (entry != NULL)
-      if(isdigit(entry->d_name[0])){
-          switch(entry->d_name[14]){
-              case 'A':
-              copyFile(entry->d_name,a+entry->d_name);
-              lines.push_back(a+entry->d_name+A);
-                // ofs<<"a/"<<entry->d_name<<",1,0,0"<<endl; //"<<"で流し込むだけ 
-                break;
-              case 'B':
-              copyFile(entry->d_name,b+entry->d_name);
-              lines.push_back(b+entry->d_name+B);
-                // ofs<<"b/"<<entry->d_name<<",0,1,0"<<endl; //"<<"で流し込むだけ 
-                break;
-              case 'C':
-              copyFile(entry->d_name,c+entry->d_name);
-              lines.push_back(c+entry->d_name+C);
-                // ofs<<"c/"<<entry->d_name<<",0,0,1"<<endl; //"<<"で流し込むだけ 
-              break;
-                default:
-                break;
-          }
-      }
-  } while (entry != NULL);
-  random_shuffle(lines.begin(), lines.end());
-  for(int i = 0 ; i<lines.size();i++){
-    ofs<<lines.at(i)<<endl;
-  }
-  return 0;
+	entry = readdir(dp);
+	if (entry != NULL)
+		if(isdigit(entry->d_name[0])){
+			if(strstr(entry->d_name,"A")!=NULL){
+				copyFile(entry->d_name,a_path+entry->d_name);
+				lines.push_back(a_path+entry->d_name+a_vector);
+			}else if (strstr(entry->d_name,"B")!=NULL){
+				copyFile(entry->d_name,b_path+entry->d_name);
+				lines.push_back(b_path+entry->d_name+b_vector);
+			}else if (strstr(entry->d_name,"C")!=NULL){
+				copyFile(entry->d_name,c_path+entry->d_name);
+				lines.push_back(c_path+entry->d_name+c_vector);
+			}
+		}
+	} while (entry != NULL);
+	random_shuffle(lines.begin(), lines.end());
+	for(int i = 0 ; i<lines.size();i++){
+		ofs<<lines.at(i)<<endl;
+	}
+	return 0;
 }
